5042612a3e5d33cf342ea9fdb2f1f63bef5c1d37
Naufalahmadfauz <naufalahmadfauz@gmail.com>
2019-11-03 21:21:57 +0700

    fix forecast.js and undefined destructure geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request');
 const geocode = (address,callback)=>{
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoibmF1ZmFsMzciLCJhIjoiY2syN211aXNuMGFqZjNsbXNvZjJib3pobiJ9.OWpi3191PjrmIAVe38LQmw&limit=1'
 
-    request({url, json:true},(error,{body})=>{
+    request({url, json:true},(error,{body}={})=>{
 
         if (error){
             callback('Unable To Connect!',undefined)
